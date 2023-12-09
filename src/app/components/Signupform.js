aed387ae2752d7b7f2a72c9fae3c695ffb52b21e
amishk783 <amishkumar800@gmail.com>
2023-12-10 01:33:04 +0530

    Made website responsive

--- src/app/components/Signupform.js
+++ src/app/components/Signupform.js
@@ -2,28 +2,31 @@ import React from "react";
 
 const Signupform = () => {
   return (
-    <div className="border-2 border-y-rose-50 rounded-md m-10 w-[500px] p-5">
+    <div className="border-2 border-y-rose-50 rounded-md md:m-10 md:w-[500px] m-5 items-center">
       <h1 className="pt-3 pl-3 relative inline-block">
         Hurry 35% Off On New Tata Play Connection
         <span className=" ml-3 absolute bottom-0 left-0 w-1/2 border-b-2"></span>
       </h1>
       <h3 className="pt-3 pl-3">Enter Contact Details *</h3>
-      <form className="flex flex-wrap">
-        <div className="flex flex-col md:flex-row md:space-x-2  w-1/2 md:w-auto gap-3">
+      <form className="flex flex-wrap items-center justify-center">
+        <div className="w-full flex flex-col md:flex-row md:space-x-2 md:w-full gap-3 md:pr-4">
           <input
-            className="border-2 rounded-full m-2 px-2 py-2"
+            className="border-2 rounded-full m-4 p-2 md:w-1/2  text-center md:text-start"
             placeholder="Full Name"
           />
           <input
-            className="border-2 rounded-full m-2 px-2 py-2"
+            className="border-2 rounded-full m-4 p-2 md:w-1/2 text-center md:text-start "
             placeholder="Mobile Number"
           />
         </div>
         <input
-          className="w-full border-2 md:auto rounded-full m-2 px-2 py-2"
+          className="w-full border-2 rounded-full m-4 px-3 py-2  text-center md:text-start"
           placeholder="Please Share Your Pin Code"
         />
-        <button className=" bg-orange-200 w-1/2 rounded-md m-2 px-2 py-2">Send</button>
+        
+          <button className=" bg-orange-200 w-1/2 rounded-md m-2 px-2 py-2 ">
+            Send
+          </button>
       </form>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -10,30 +10,30 @@ import { Info } from "@/constants";
 export default function Home() {
   return (
     <Fragment>
-      <header className="flex justify-around py-3 items-center align-middle bg-yellow-100">
+      <header className="flex justify-center md:justify-around py-3 items-center align-middle bg-yellow-100">
         <Link href={""}>
           <Image src="/logo1.jpg" alt="logo" width={100} height={50} />
         </Link>
-        <div className="flex gap-10">
+        <div className="hidden md:flex gap-10">
           <h3 className="hover:text-cyan-300">Tata Play DTH Connection</h3>
           <h3 className="hover:text-cyan-300"> Dish Tv New connection</h3>
         </div>
-        <button>Call us</button>
+        <button className="hidden md:block md:text-lg bg-red-600 m-2 px-4 py-2 rounded-md text-slate-50 text-center">Call us</button>
       </header>
       <main className="flex flex-col items-center justify-between pt-10">
         <div className="z-10 w-full items-center justify-center font-mono text-sm lg:flex h-2/3">
           <div className=" items-center mt-5 justify-center">
-            <h1 className=" text-center text-3xl">Tata Play Network</h1>
+            <h1 className="text-center text-3xl">Tata Play Network</h1>
             <h3 className=" text-center text-2xl">1299</h3>
 
-            <div className="flex gap-24 mt-10">
+            <div className="flex flex-col md:gap-24 mt-10 md:flex-row items-center">
               <Signupform />
               <Image
                 src="/Setupbox.png"
                 alt="show"
                 width={400}
                 height={100}
-                className="p-8"
+                className="p-8 md:w-[300px] h-[250px]"
               />
             </div>
           </div>
@@ -41,16 +41,15 @@ export default function Home() {
         <div className="mt-10">
           <div className="flex flex-col items-center gap-5">
             <h1 className=" text-center text-4xl">
-              {" "}
               Choose your Set Top Box now.
             </h1>
-            <h2 className="text-xl">
+            <h2 className=" text-sm px-5 text-center md:text-xl">
               View our range of Set Top Boxes available with different
               specifications
             </h2>
             <h1 className="text-center text-3xl">1 Month Offer</h1>
           </div>
-          <div className="grid text-center mt-10 lg:max-w-5xl  lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left gap-10">
+          <div className="grid m-6 text-center mt-10 lg:max-w-5xl  lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left gap-10">
             {dthinfo.map((dth) => (
               <CardDth
                 key={dth.plan}
@@ -63,18 +62,18 @@ export default function Home() {
         </div>
         <div className="mt-16 text-center text-3xl font-mono">
           <div>
-            <h1>Why to Choose Us</h1>
+            <h1>Why Choose Us</h1>
           </div>
 
-          <div className="grid text-center mt-10 lg:max-w-5xl  lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left gap-10">
+          <div className="grid m-6 text-center mt-10 lg:max-w-5xl  lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left gap-10">
             {Info.map((info) => (
               <CardDetail key={info.title} title={info.title} img={info.img} />
             ))}
           </div>
         </div>
       </main>
-      <footer className="">
-        <div className="mt-10 bg-black flex justify-center gap-5">
+      <footer className="bg-black ">
+        <div className="mt-10 flex flex-col justify-center md:gap-5 items-center">
           <h1 className=" text-gray-50">Privacy Policy</h1>
           <h2 className=" text-gray-50 "> Terms & Conditions</h2>
         </div>
